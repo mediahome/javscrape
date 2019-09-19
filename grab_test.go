45d2acba665ce554843733e45a0c69655e8973d7
godcong <jumbycc@163.com>
2019-09-19 16:51:48 +0800

    create scrape

--- grab_test.go
+++ grab_test.go
@@ -5,13 +5,17 @@ import "testing"
 // TestGrabBP4X_Find ...
 func TestGrabBP4X_Find(t *testing.T) {
 	grab := NewGrabBP4X(BP4XTypeJAV)
-	err := grab.Find("abp-874")
+	doc, err := grab.Find("abp-874")
+	msg := new(Message)
+	err = doc.Decode(msg)
 	t.Log(err)
 }
 
 // TestGrabJAVBUS_Find ...
 func TestGrabJAVBUS_Find(t *testing.T) {
 	grab := NewGrabJAVBUS(LanguageJapanese)
-	err := grab.Find("Q8180")
+	doc, err := grab.Find("Q8180")
+	msg := new(Message)
+	err = doc.Decode(msg)
 	t.Log(err)
 }
