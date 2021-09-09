b56082e62d6c9738016623cf1d7e9af6db4e3f9b
godcong <jumbycc@163.com>
2021-09-09 16:13:49 +0800

    update

--- cache/cache.go
+++ cache/cache.go
@@ -1,11 +1,10 @@
-package scrape
+package cache
 
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
-	cache "github.com/gocacher/badger-cache/v3"
-	"github.com/goextension/log"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,7 +13,10 @@ import (
 	"sync"
 
 	"github.com/PuerkitoBio/goquery"
+	cache "github.com/gocacher/badger-cache/v3"
 	"github.com/gocacher/cacher"
+
+	"github.com/javscrape/go-scrape/network"
 )
 
 // DefaultCachePath ...
@@ -24,8 +26,8 @@ var _cacheOnce *sync.Once
 
 // Cache ...
 type Cache struct {
-	lock  sync.Mutex
-	cache cacher.Cacher
+	lock sync.Mutex
+	cacher.Cacher
 }
 
 func init() {
@@ -35,7 +37,7 @@ func init() {
 func newCache() *Cache {
 	cache.DefaultCachePath = DefaultCachePath
 	return &Cache{
-		cache: cache.New(),
+		Cacher: cache.New(),
 	}
 }
 
@@ -50,7 +52,7 @@ func NewCache() *Cache {
 // Hash ...
 func Hash(url string) string {
 	sum256 := sha256.Sum256([]byte(url))
-	return fmt.Sprintf("%x", sum256)
+	return hex.EncodeToString(sum256[:])
 }
 
 // GetReader ...
@@ -63,43 +65,39 @@ func (c *Cache) GetReader(url string, force bool) (io.Reader, error) {
 }
 
 // GetBytes ...
-func (c *Cache) GetBytes(url string) ([]byte, error) {
-	return c.Get(url)
+func (c *Cache) GetBytes(url string, force bool) ([]byte, error) {
+	return c.get(url, force)
 }
 
-func (c *Cache) ForceGet(url string) (bys []byte, e error) {
-	return c.get(url, false)
+func (c *Cache) HasURL(url string) bool {
+	return c.has(Hash(url))
 }
 
-// Get ...
-func (c *Cache) Get(url string) (bys []byte, e error) {
-	return c.get(url, true)
+func (c *Cache) has(name string) bool {
+	exist, err := c.Has(name)
+	return err == nil && exist
 }
 
 func (c *Cache) get(url string, useCache bool) (bys []byte, e error) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
 	name := Hash(url)
 	if useCache {
-		b, e := c.cache.Has(name)
-		log.Infow("cache get", "url", url, "hash", name, "exist", b)
-		if e == nil && b {
-			getted, e := c.cache.Get(name)
+		b := c.has(name)
+		//log.Infow("cache get", "url", url, "hash", name, "exist", b)
+		if b {
+			bys, e = c.Get(name)
 			if e != nil {
 				return nil, e
 			}
-			return getted, nil
+			return bys, nil
 		}
 	}
-	if cli == nil {
-		cli = http.DefaultClient
-	}
-
+	cli := network.Client()
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.11 Safari/537.36")
+	req.Header.Set("user-agent",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.11 Safari/537.36")
 
 	res, e := cli.Do(req)
 	if e != nil {
@@ -113,7 +111,7 @@ func (c *Cache) get(url string, useCache bool) (bys []byte, e error) {
 	if e != nil {
 		return nil, e
 	}
-	e = c.cache.Set(name, bys)
+	e = c.Set(name, bys)
 	if e != nil {
 		return nil, e
 	}
@@ -151,8 +149,8 @@ func (c *Cache) Query(url string, force bool) (*goquery.Document, error) {
 	return goquery.NewDocumentFromReader(closer)
 }
 
-func (c *Cache) BaseQuery(url string) (*goquery.Document, error) {
-	closer, e := c.GetReader(url, true)
+func (c *Cache) GetQuery(url string, force bool) (*goquery.Document, error) {
+	closer, e := c.GetReader(url, force)
 	if e != nil {
 		return nil, e
 	}
