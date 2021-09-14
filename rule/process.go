2584d6082ca66caf464f9a5a18947f53a4c89cd6
godcong <jumbycc@163.com>
2021-09-14 16:59:16 +0800

    update

--- rule/process.go
+++ rule/process.go
@@ -3,8 +3,7 @@ package rule
 type Process struct {
 	Name          string       `toml:"name,omitempty"`
 	Selector      string       `toml:"selector,omitempty"`
-	Compare       []Process    `toml:"compare,omitempty"`
-	Index         int          `toml:"index,omitempty"`
+	Compare       string       `toml:"compare,omitempty"`
 	Type          ProcessType  `toml:"type,omitempty"`
 	Property      string       `toml:"property,omitempty"`
 	PropertyIndex int          `toml:"property_index,omitempty"`
